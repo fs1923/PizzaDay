b83f1fafe6b900de38d0248a51d8dd80bcf2fb28
Ihor Barmak <paulbrmk@gmail.com>
2017-04-03 12:45:17 +0300

    added slingshot

--- PizzaDay/imports/ui/InsertGroup.jsx
+++ PizzaDay/imports/ui/InsertGroup.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { FormGroup, FormControl, Button, ControlLabel } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
+import { Slingshot } from 'meteor/edgee:slingshot';
 
 export default class InsertGroup extends Component {
     insertGroup(event) {
@@ -18,12 +19,20 @@ export default class InsertGroup extends Component {
     };
     uploadFile(event) {
         event.preventDefault();
-        var files = $("input.file_bag")[0].files;
-        S3.upload({
-                files:files,
-                path:"/images/"
-            },function(e,r){
-                console.log(e,r);
+        let files = $("input.file_bag")[0].files;
+
+
+        var uploader = new Slingshot.Upload("myFileUploads");
+
+        uploader.send(files[0], function (error, downloadUrl) {
+            if (error) {
+            // Log service detailed response
+                console.error('Error uploading', error);
+                // alert (error);
+            }
+            else {
+                console.log(downloadUrl)
+            }
         });
     }
     render() {
--- PizzaDay/server/main.js
+++ PizzaDay/server/main.js
@@ -1,1 +1,4 @@
+import '/imports/startup/server/slingshot.js';
+import '/imports/startup/slingshot.js';
 import '../imports/api/groups.js';
+// import '/imports/'
